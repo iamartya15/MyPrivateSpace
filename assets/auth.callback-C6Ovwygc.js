346bc65ec463fca4d86defdbbb6e4baa75f7a8f6
Amartya Kushwaha <139534355+iamartya15@users.noreply.github.com>
2026-05-25 20:47:28 +0530

    Deploy production build to GitHub Pages

---
+++ assets/auth.callback-C6Ovwygc.js
@@ -0,0 +1,1 @@
+import{u as n,c as a,j as e}from"./index-DJPdOI4l.js";function s(){const t=n();return a.useEffect(()=>{t({to:"/"})},[t]),e.jsx("div",{children:"Signing you in..."})}export{s as component};
